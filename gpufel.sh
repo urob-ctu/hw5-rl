7bc96a1011661cf6c3ed41c66b726e9e79312a8c
Michal Mrkos <majkl.mrkos@seznam.cz>
2025-12-07 01:09:48 +0100

    last chance

--- gpufel.sh
+++ gpufel.sh
@@ -2,36 +2,44 @@
 mkdir -p /mnt/scratch/temporary/$USER/vscode-server
 mkdir -p /mnt/scratch/temporary/$USER/cache
 mkdir -p /mnt/scratch/temporary/$USER/envs
+mkdir -p /mnt/scratch/temporary/$USER/dotfiles_storage/cache
 
-# 2. Delete existing heavy folders in HOME (Make sure you don't need data from them)
-rm -rf ~/.vscode-server
-rm -rf ~/.cache
-# (Don't delete your current project source code, just the envs)
+# Force pip to use scratch for cache
+export XDG_CACHE_HOME="/mnt/scratch/temporary/$USER/dotfiles_storage/cache"
+export PIP_CACHE_DIR="$XDG_CACHE_HOME/pip"
+
+# 2. Clean up old environments
+rm -rf /mnt/scratch/temporary/$USER/envs/gpufel-env
+rm -f ./gpufel-env
 
 # 3. Create Symbolic Links (The Magic Step)
+# CRITICAL: Remove the folder from HOME first, otherwise linking fails
+rm -rf ~/.vscode-server 
 ln -s /mnt/scratch/temporary/$USER/vscode-server ~/.vscode-server
-ln -s /mnt/scratch/temporary/$USER/cache ~/.cache
 
-# 4. Create your virtual environment DIRECTLY in scratch
-python3 -m venv /mnt/scratch/temporary/$USER/envs/gpufel-env
-
-# 5. Create a convenient link to the env in your project folder
-ln -s /mnt/scratch/temporary/$USER/envs/gpufel-env ./gpufel-env
-
-
-ml PyTorch
-ml JupyterNotebook
+# 4. Load Modules
+# If this crashes with "Illegal Instruction", switch to: PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
+ml OpenSSL
+ml PyTorch/2.3.0-foss-2023b-CUDA-12.4.0 
 echo 'Loaded modules'
+
+# 5. Create Env
 python -m venv --system-site-packages /mnt/scratch/temporary/$USER/envs/gpufel-env
+ln -s /mnt/scratch/temporary/$USER/envs/gpufel-env ./gpufel-env
 source /mnt/scratch/temporary/$USER/envs/gpufel-env/bin/activate
 echo 'Activated virtual environment'
-pip install --upgrade pip
-pip install -r requirements.txt
+
+# 6. Install
+# Added --no-cache-dir as double safety
+pip install --upgrade pip --no-cache-dir
+pip install -r gpufel_reqs.txt --no-cache-dir
 
 echo 'Installed required packages'
 echo 'running tests'
+
 python -m pytest -s
 
-
 echo 'Starting Jupyter Notebook server'
+# Unset EGL so Jupyter can use the window if needed
+unset MUJOCO_GL
 jupyter notebook --no-browser --ip=0.0.0.0 --port=8888
